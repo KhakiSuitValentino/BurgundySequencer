5d387c30374970294f2589bd4fb4c2524f2a29a5
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-10-28 14:22:30 -0400

    updated dimensions for buttons

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 import os
 
+#Globals:
 WIDTH, HEIGHT = 922, 692
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PYGAME SEQUENCER")
+pygame.display.set_caption("BURGUNDY SEQUENCER")
 
-FPS = 60
+FPS = 10
+BUTTON_CLICKBOX = 40, 40
 
+#Images:
 BACKGROUND_IMG = pygame.image.load(os.path.join('assets', 'layout.png'))
+BUTTON = pygame.image.load(os.path.join('assets', "button.png"))
 
 def draw_window():
      pygame.display.update()
      WIN.blit(BACKGROUND_IMG, (0, 0))
+     WIN.blit(BUTTON, (277, 24))
+     WIN.blit(BUTTON, (347, 64))
 
 
 #THE GAME LOOP:
